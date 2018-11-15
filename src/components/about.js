fdec746c14c4dead69d31702081b6fc80414dae9
Stepan Novozhilov <hit2hat@icloud.com>
2018-11-15 22:16:48 +0300

    Fix credentials

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div id="about">
             <h2>WarMap [DEMO]</h2>
-            <h5>Author: <a rel="noopener noreferrer" href="https://vk.me/hit2hat" target="_blank">@hit2hat</a></h5>
+            <h5>Author: <a rel="noopener noreferrer" href="https://vk.me/hit2hat" target="_blank" style={{color: 'white'}}>@hit2hat</a></h5>
         </div>
     );
 };
